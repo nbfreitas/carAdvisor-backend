c5ad62e293071f6752687eb3e21358cfe0778533
Nicolas Freitas <nicolasfreitas@Nicolass-MacBook-Pro.local>
2019-10-26 20:37:31 -0300

    add feature: appointments

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ import CarController from './controllers/CarController';
 import SessionController from './controllers/SessionController';
 import FileController from './controllers/FileController';
 import ProviderController from './controllers/ProviderController';
+import AppointmentController from './controllers/AppointmentController';
+import ScheduleController from './controllers/ScheduleController';
 
 import authMiddleware from './middlewares/auth';
 
@@ -26,6 +28,11 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
