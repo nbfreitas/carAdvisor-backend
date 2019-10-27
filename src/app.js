a32d4f09dae35b9bd1c0d69ac3a2935414b81bf6
Nicolas Freitas <nicolasfreitas@Nicolass-MacBook-Pro.local>
2019-10-27 12:32:52 -0300

    add feature: exception handling

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -10,14 +14,18 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
     const app = require('http').Server(this.server);
     const io = require('socket.io')(app);
 
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
     this.server.use(express.json());
     // this.server.use(
@@ -39,6 +47,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.requestHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
